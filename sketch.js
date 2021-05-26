0c1c891c3738aa44905310420231547537d0aa7a
Pratham24 <81266140+Pratham24@users.noreply.github.com>
2021-05-26 19:02:52 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var backgroundImg
+var bg
+var engine, world
+function preload(){
+  getBackgroundImg();
+}
+function setup() {
+  createCanvas(800,400);
+}
+
+function draw() {
+  if(backgroundImg)
+        background(backgroundImg);
+}
+
+async function getBackgroundImg(){
+  var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+  var responseJSON = await response.json();
+
+  var datetime = responseJSON.datetime;
+  var hour = datetime.slice(11,13);
+  
+  if(hour>=07 && hour<=19){
+      bg = "snow1.jpg";
+  }
+  else{
+      bg = "snow3.jpg";
+  }
+
+  backgroundImg = loadImage(bg);
+  console.log(backgroundImg);
+}
